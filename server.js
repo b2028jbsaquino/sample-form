89e9a3d28821920ffe049a9f60a07a803d5893ab
Glitch (rounded-cool-bait) <none>
2025-02-25 00:16:20 +0000

    🌡️⚽ Updated with Glitch

--- server.js
+++ server.js
@@ -16,38 +16,10 @@ app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Sets a basic route index.hbs when website initially starts and when home is clicked from the nav bar or whenever a process needs to go back to home 
-app.get('/process', (req, res) => {
+app.post('/submit', (req, res) => {
     const formData = req.query;
-    formData.gender = determineGender(formData.gender);
-    formData.talent = determineTalent(formData.talent);
     res.render('pages/acceptFormData.hbs', {formData});
 })
-
-function determineGender(gender) {
-  let dGender="";
-  if (gender == "f")
-       dGender = "Female"
-  else if (gender=="m")
-     dGender = "Male"
-  else
-     dGender = "Prefer Not to Say"
-  return dGender 
-}
-
-function determineTalent(talents) {
-  for (let i in talents) {
-    console.log(talents[i])
-    if (talents[i] == 'd')
-      talents[i] = "Dance"
-    else if (talents[i] == 's')
-      talents[i] = "Sing"
-    else
-      talents[i] = "Play Instruments"
-  }
-  console.log(talents)
-  return talents 
-}
-
 
 // Step 5: Start HTTP Server on a port number 3000
 // This will create a web service for your own project
